eb7e419bf299f829d0a6e043cad417f2325c62c6
Ashtonaut <ashtonautica@gmail.com>
2024-01-18 18:30:26 +0000

    Add grid size change functionality to button

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 const container = document.querySelector("#gridContainer");
 const gridSquares = [];
+let gridSize = 16;
 
-for (let y = 0; y < 16; y++) {
-    const ydiv = document.createElement("div");
-    ydiv.id = `y${y}`;
-    ydiv.classList.add("row");
-    container.appendChild(ydiv);
-
-    for (let x = 0; x < 16; x++) {
-        const xdiv = document.createElement("div");
-        xdiv.id = `x${x}`;
-        xdiv.classList.add("gridSquare");
-        ydiv.appendChild(xdiv);
-        gridSquares.push(xdiv);
+function constructNewGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
+
+    for (let y = 0; y < gridSize; y++) {
+        const row = document.createElement("div");
+        row.id = `row${y}`;
+        row.classList.add("row");
+        container.appendChild(row);
+
+        for (let x = 0; x < gridSize; x++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.id = `gridSquare${x}`;
+            gridSquare.classList.add("gridSquare");
+            row.appendChild(gridSquare);
+            gridSquares.push(gridSquare);
+        }
+    }
+
+    gridSquares.forEach(gridSquare => {
+        gridSquare.addEventListener("mouseenter", event => {
+            gridSquare.style.backgroundColor = "black";
+        });
+    });
 }
 
-gridSquares.forEach(gridSquare => {
-    gridSquare.addEventListener("mouseenter", event => {
-        gridSquare.style.backgroundColor = "black";
-    });
-});
+const button = document.querySelector("button");
+button.addEventListener("click", event => {
+    gridSize = prompt("Please type in the desired number of squares per side of the square grid (maximum 100).", 16);
+    if (gridSize > 100) {
+        gridSize = 100
+    };
+    constructNewGrid();
+})
+
+constructNewGrid();
